425252eac2b51afb46f1fda0733d011f1b903850
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2024-05-10 10:47:49 +0300

    make build

---
+++ index.36276ecf.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=(e.clientHeight-t.clientHeight)/2,l=(e.clientWidth-t.clientWidth)/2;t.style.left="".concat(c,"px"),t.style.top="".concat(l,"px");
+//# sourceMappingURL=index.36276ecf.js.map
